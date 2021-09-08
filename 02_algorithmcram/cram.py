d0d86f1e367dab2dd911ece837aa3a360bc1f01f
Russell Tran <tranrl@stanford.edu>
2021-09-07 22:45:08 -0700

    Add mergesort

--- 02_algorithmcram/cram.py
+++ 02_algorithmcram/cram.py
@@ -6,7 +6,7 @@ def quicksort(arr):
 
 
 class Solution:
-    def binary_search(self, nums: List[int], target: int) -> int:
+    def binary_search(self, nums, target):
         if len(nums) == 0:
             return -1
         if len(nums) == 1:
@@ -29,3 +29,50 @@ class Solution:
 def dfs(root):
 	# use a stack whereas bfs uses a queue
 	return None
+
+def mergeSort(arr):
+	# https://www.geeksforgeeks.org/merge-sort/
+	if len(arr) <= 1:
+		return
+	middle = len(arr) // 2
+	left_arr = arr[:middle] # copy
+	right_arr = arr[middle:] # copy
+	mergeSort(left_arr)
+	mergeSort(right_arr)
+	i = 0
+	j = 0
+	k = 0
+	# merge by comparison (put the lesser ones in first)
+	while i < len(left_arr) and j < len(right_arr):
+		if left_arr[i] < right_arr[j]:
+			arr[k] = left_arr[i]
+			i += 1
+		else:
+			arr[k] = right_arr[j]
+			j += 1
+		k += 1
+
+	# comparison is done; put all the rest in
+	while i < len(left_arr):
+		arr[k] = left_arr[i]
+		i += 1
+		k += 1
+	while j < len(right_arr):
+		arr[k] = right_arr[j]
+		j += 1
+		k += 1
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+ 
+ 
+# Driver Code
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
