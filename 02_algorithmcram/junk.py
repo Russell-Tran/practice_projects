30b3e44317a99a0500c298f8d8ebabeb9950ba30
Russell Tran <tranrl@stanford.edu>
2021-11-10 18:54:37 -0800

    add junk

--- 02_algorithmcram/junk.py
+++ 02_algorithmcram/junk.py
@@ -32,3 +32,51 @@ def reverse(self, head):
 		curr.next = reverse
 		prev = curr
 		curr = nxt
+
+class Node:
+	def __init__(self, val = 0, neighbors = None):
+		self.val = val
+		self.neighbors = neighbors if neighbors else []
+
+def clonegraph(node): 
+	if not node:
+		return None
+	seen = set()
+	mappings = {}
+	stack = [node]
+	while len(stack) > 0:
+		curr = stack[-1]
+		stack.pop(-1)
+		seen.add(curr)
+		if curr not in mappings:
+			mappings[curr] = Node(curr.val)
+		mirrored_curr = mappings[curr]
+		for neighbor in curr.neighbors:
+			if neighbor not in mappings:
+				mappings[neighbor] = Node(neighbor.val)
+				mirrored_neighbor = mappings[neighbor]
+				if mirrored_neighbor not in mirrored_curr.neighbors:
+					mirrored_curr.neighbors.append(mirrored_neighbor)
+				if neighbor not in seen:
+					stack.append(neighbor)
+	return mappings[node]
+
+class Solution:
+	def generateParenthesis(self, n):
+		self.output = []
+		self.recurse("", n, n)
+		return self.output
+
+	def recurse(self, curr, num_open, num_close):
+		if num_close == 0:
+			self.output.append(curr)
+			return
+
+		if num_close > num_open:
+			self.recurse(curr + ")", num_open, num_close-1)
+
+		if num_open > 0:
+			self.recurse(curr + "(", num_open-1, num_close)
+
+
+
